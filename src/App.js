24e2924910dd56b02fc94cd97f3471e8fe8988b1
NMarti20 <natasha.martinezsj@gmail.com>
2022-01-17 18:39:08 -0800

    added mp4 file and created functions for get req

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
+import axios from 'axios';
 import Header from "./components/Header/Header";
+import Mainvideo from './assets/earthVideo.mp4'
 
-const App = () => {
-  return (
-    <div className="main">
-      <Header />
-    </div>
-  );
+const baseURL = `https://api.nasa.gov/planetary/apod?api_key=AJV0AFfyYaPdUzakNA5eJH7eNnuy3EKux0PFiPrG`
+
+class App extends Component {
+  state = {
+    date: "",
+    photo: ""
+  };
+
+  updateDate = e => {
+    e.preventDefault();
+    let dateFromInput = e.target[0].value;
+    this.setState({ date: dateFromInput });
+    this.getPicture(dateFromInput);
+  };
+
+  getPicture = date => {
+    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=aA53jfDx54NAdobPRkGVM0aYu7uex2u6KhWI3CBV`)
+      .then(response => response.json())
+      .then(photoData => this.setState({ photo: photoData }));
+  };
+
+
+  render() {
+    return (
+      <div className="main">
+        <Header />
+        {/* <GetDate updateDate={this.updateDate} /> */}
+        <video className="app__video" src={Mainvideo} autoPlay muted loop />
+        {/* <Picture photo={this.state.photo} /> */}
+      </div>
+    );
+  }
 };
 
 export default App;
---
+++ src/components/GetDate/GetDate.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './_getDate.scss'
+
+
+
+const GetDate = props => (
+    <form className="form" onSubmit={props.updateDate}>
+        <label className="form__label">Enter a date (YYYY-MM-DD) :</label>
+        <input />
+        <button className="form__button" type="submit" >SUBMIT</button>
+    </form>
+);
+
+export default GetDate;
